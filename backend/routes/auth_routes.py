0439c046fe745fc2e882c311fbcf2115a9643e39
Parth <127748701+Parth301@users.noreply.github.com>
2025-04-18 23:33:13 +0530

    Update auth_routes.py

--- backend/routes/auth_routes.py
+++ backend/routes/auth_routes.py
@@ -5,7 +5,7 @@ from flask import Blueprint, request, jsonify
 import mysql.connector
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
-from backend.db import get_db_connection
+from db import get_db_connection
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, 
