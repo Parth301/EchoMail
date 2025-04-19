5844a4c445c25916ae1b5a960e646bfdd5b1323a
Parth <127748701+Parth301@users.noreply.github.com>
2025-04-19 20:13:42 +0530

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -1,12 +1,28 @@
-import os
 import pymysql
 
 def get_db_connection():
-    return pymysql.connect(
-        host=os.environ["buvdpvm918jfonwzvibq-mysql.services.clever-cloud.com"],
-        user=os.environ["udmktifdo3wwwnth"],
-        password=os.environ["LpCIiJ9bFdx2eekWOI4K"],
-        database=os.environ["buvdpvm918jfonwzvibq"],
-        port=int(os.environ.get("DB_PORT", 3306)),
+    return pymysql.connect( 
+        host="buvdpvm918jfonwzvibq-mysql.services.clever-cloud.com",
+        user="udmktifdo3wwwnth",
+        password="LpCIiJ9bFdx2eekWOI4K",
+        database="buvdpvm918jfonwzvibq",
+        port=3306,
         cursorclass=pymysql.cursors.DictCursor
     )
+
+try:
+    connection = get_db_connection()
+    print("Connection Successful!")
+
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT email FROM user")
+        users = cursor.fetchall()
+
+        print("User Emails:")
+        for user in users:
+            print(user["email"])
+
+    connection.close()
+
+except Exception as e:
+    print(f"Error: {e}")
