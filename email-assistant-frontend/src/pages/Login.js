7f475e6b79dc7659f159c1456a1b1c0323b12b9f
Parth <127748701+Parth301@users.noreply.github.com>
2025-04-19 22:22:20 +0530

    Update Login.js

--- email-assistant-frontend/src/pages/Login.js
+++ email-assistant-frontend/src/pages/Login.js
@@ -1,6 +1,6 @@
 import React, { useState, useCallback, useRef, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import axios from 'axios';
+import api from 'axios';
 import {
   Container,
   TextField,
