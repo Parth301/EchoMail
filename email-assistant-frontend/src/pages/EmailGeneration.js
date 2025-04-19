db04f2bffecc2537939cc33b09c41c86c090a454
Parth <127748701+Parth301@users.noreply.github.com>
2025-04-19 22:48:55 +0530

    Update EmailGeneration.js

--- email-assistant-frontend/src/pages/EmailGeneration.js
+++ email-assistant-frontend/src/pages/EmailGeneration.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useMemo, useCallback } from "react";
-import axios from "axios";
+import api from 'axios';
 import { useNavigate } from "react-router-dom";
 import { 
   Box, 
