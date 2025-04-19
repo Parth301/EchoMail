f7501bcb9842d398ebd51b27213d0e3c32ff8cfe
Parth <127748701+Parth301@users.noreply.github.com>
2025-04-19 17:51:27 +0530

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -1,11 +1,12 @@
 import os
 import pymysql
-db_config = {
-    "host": os.environ.get("DB_HOST", "localhost"),
-    "user": os.environ.get("DB_USER", "root"),
-    "password": os.environ.get("DB_PASSWORD", ""),
-    "database": os.environ.get("DB_NAME", "email_assistant"),
-    "cursorclass": pymysql.cursors.DictCursor  # Optional: returns rows as dictionaries
-}
+
 def get_db_connection():
-    return pymysql.connect(**db_config)
+    return pymysql.connect(
+        host=os.environ["sql12.freesqldatabase.com"],
+        user=os.environ["sql12774147"],
+        password=os.environ["1GJN1Weipi"],
+        database=os.environ["sql12774147"],
+        port=int(os.environ.get("DB_PORT", 3306)),
+        cursorclass=pymysql.cursors.DictCursor
+    )
