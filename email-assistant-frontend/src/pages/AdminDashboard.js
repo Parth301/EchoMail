5ed847b142c511031cc7b3c805455bfb92064b26
Parth <127748701+Parth301@users.noreply.github.com>
2025-04-19 22:22:00 +0530

    Update AdminDashboard.js

--- email-assistant-frontend/src/pages/AdminDashboard.js
+++ email-assistant-frontend/src/pages/AdminDashboard.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useMemo } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from 'axios';
+import api from 'axios';
 import {
   Container,
   Typography,
